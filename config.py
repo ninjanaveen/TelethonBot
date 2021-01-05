93201693bee2f7b1edaaf0982c0e8fe538300018
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2021-01-05 09:40:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    BANNED_ID = os.environ.get("BANNED_ID", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     API_HASH = os.environ.get("API_HASH", None)
     API_ID = int(os.environ.get("APP_ID", 6))
